5802d6001347ce60b38926211a48c627a550fcb1
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-03-11 15:42:53 +0530

    add support for custom lifetime

--- weeks4k.py
+++ weeks4k.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
 import sys
 
+try:
+    weeks_num = int(sys.argv[4])
+except Exception:
+    weeks_num = 4000
+
 birth = datetime(year=int(sys.argv[1]), month=int(sys.argv[2]), day=int(sys.argv[3]))
 year_birth = birth.year
 start = datetime(year=year_birth + 1, month=1, day=1)
@@ -25,7 +30,7 @@ week_now = now.day // 7
 row_cur = f"{str_month_done * (now.month - 1)} {char_done * week_now}{char_undone * (4 - week_now)}{str_month_undone * (12 - now.month)}"
 print(f"{now.year}{row_cur}")
 
-days_undone = 4000 * 7 - (now - start).days
+days_undone = weeks_num * 7 - (now - start).days
 date_end = now + timedelta(days=days_undone)
 years_undone = date_end.year - now.year
 for idx in range(years_undone):
