7468edb229da3f59b0a5cbc5fe3e9c549ceefaeb
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-03-11 23:52:06 +0530

    simplify code; more generic, robust

--- weeks4k.py
+++ weeks4k.py
@@ -3,57 +3,56 @@ import datetime
 import functools
 import os
 import sys
+import typing
 
 
-weeks_num = int(os.environ.get("WEEKS", "4000"))
-labels = bool(os.environ.get("LABELS", ""))
+WEEKS = int(os.environ.get("WEEKS", "4000"))
+LABELS = bool(os.environ.get("LABELS", ""))
 
-birth = datetime.datetime(year=int(sys.argv[1]), month=int(sys.argv[2]), day=int(sys.argv[3]))
-start = datetime.datetime(year=birth.year + 1, month=1, day=1)
-now = datetime.datetime.now()
-assert(birth < now)
+WEEKS_IN_MONTH = 4
+DAYS_IN_WEEK = 7
 
-months_in_year = 12
-weeks_in_month = 4
-days_in_week = 7
+BEGIN = datetime.datetime(
+    year=int(sys.argv[1]), month=int(sys.argv[2]), day=int(sys.argv[3])
+)
+END = BEGIN + datetime.timedelta(days=WEEKS * DAYS_IN_WEEK)
+NOW = datetime.datetime.now()
 
-char_done = "\u2588"
-str_month_done = f" {char_done * weeks_in_month}"
-str_year_done = str_month_done * months_in_year
-char_undone = "\u00B7"
-str_month_undone = f" {char_undone * weeks_in_month}"
-str_year_undone = str_month_undone * months_in_year
 
-if labels:
-    print(functools.reduce(lambda s, m: f"{s}  {m.upper()}", calendar.month_abbr[1:], "    "))
+class Week(typing.NamedTuple):
+    year: int
+    month: int
+    week_in_month: int
 
-week_birth = birth.day // days_in_week
-week_now = now.day // days_in_week
-row_start = f"{str_month_undone * (birth.month - 1)} {char_undone * week_birth}{char_done * (weeks_in_month - week_birth)}"
-if now.year == birth.year:
-    row_start += f"{str_month_done * (now.month - birth.month - 1)} {char_done * week_now}{char_undone * (weeks_in_month - week_now)}{str_month_undone * (months_in_year - now.month)}"
-else:
-    row_start += f"{str_month_done * (months_in_year - birth.month)}"
+    @staticmethod
+    def get_week_num(day: int) -> int:
+        return min(day // DAYS_IN_WEEK, WEEKS_IN_MONTH)
 
-if labels:
-    print(birth.year, end="")
-print(row_start)
 
-for idx in range(now.year - start.year):
-    if labels:
-        print(birth.year + 1 + idx, end="")
-    print(str_year_done)
+WEEK_BEGIN = Week(BEGIN.year, BEGIN.month, Week.get_week_num(BEGIN.day))
+WEEK_END = Week(END.year, END.month, Week.get_week_num(END.day))
+WEEK_NOW = Week(NOW.year, NOW.month, Week.get_week_num(NOW.day))
 
-if now.year != birth.year:
-    row_cur = f"{str_month_done * (now.month - 1)} {char_done * week_now}{char_undone * (weeks_in_month - week_now)}{str_month_undone * (months_in_year - now.month)}"
-    if labels:
-        print(now.year, end="")
-    print(row_cur)
+CHAR_DONE = "\u2588"
+CHAR_UNDONE = "\u00b7"
 
-days_undone = weeks_num * days_in_week - (now - start).days
-date_end = now + datetime.timedelta(days=days_undone)
-years_undone = date_end.year - now.year
-for idx in range(years_undone):
-    if labels:
-        print(now.year + 1 + idx, end="")
-    print(str_year_undone)
+if LABELS:
+    print(
+        functools.reduce(
+            lambda s, m: f"{s}  {m.upper()}", calendar.month_abbr[1:], "    "
+        )
+    )
+
+for year in range(BEGIN.year, END.year + 1):
+    if LABELS:
+        print(year, end=" ")
+
+    for month in range(1, 13):
+        for week in range(0, 4):
+            week_cur = Week(year, month, week)
+            if week_cur > WEEK_NOW or week_cur > WEEK_END or week_cur < WEEK_BEGIN:
+                print(CHAR_UNDONE, end="")
+            else:
+                print(CHAR_DONE, end="")
+        print(end=" ")
+    print()
