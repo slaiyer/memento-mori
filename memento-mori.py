dc5186078292ebfa431582176d726eb070a83e67
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-04-10 09:27:40 +0530

    refactor for dependency injection

--- memento-mori.py
+++ memento-mori.py
@@ -6,13 +6,21 @@ import functools
 import os
 import sys
 import time
-from typing import Final, Generator, Iterable, Mapping, NamedTuple
+from typing import Final, Generator, Iterable, Mapping, NamedTuple, Sequence
 
+
+type STATUS_WEEK = bool
+type STATUS_MONTH = Generator[STATUS_WEEK]
+type STATUS_YEAR = Generator[STATUS_MONTH]
 
 CALENDAR_MONTH_ABBR: Final = calendar.month_abbr[1:]
 MONTHS_IN_YEAR: Final = len(CALENDAR_MONTH_ABBR)
 WEEKS_IN_MONTH: Final = 4
 DAYS_IN_WEEK: Final = len(calendar.day_name)
+RENDER_CHARS: Final = {
+    False: "\u2592",
+    True: "\u00b7",
+}
 
 
 class Week(NamedTuple):
@@ -24,150 +32,16 @@ class Week(NamedTuple):
     def get_week_num(
         *,
         day: int,
+        days_in_week: int,
+        weeks_in_month: int,
     ) -> int:
         return min(
-            day // DAYS_IN_WEEK,
-            WEEKS_IN_MONTH,
+            day // days_in_week,
+            weeks_in_month,
         )
 
 
-type STATUS_WEEK = bool
-type STATUS_MONTH = Generator[STATUS_WEEK]
-type STATUS_YEAR = Generator[STATUS_MONTH]
-
-RENDER_CHARS: Final[Mapping[STATUS_WEEK, str]] = {
-    False: "\u2592",
-    True: "\u00b7",
-}
-
-
-def compute_calendar(
-    *,
-    begin: datetime.datetime,
-    end: datetime.datetime,
-    now: datetime.datetime,
-) -> Generator[STATUS_YEAR]:
-    week_begin: Final = Week(
-        year=begin.year,
-        month=begin.month,
-        week_in_month=Week.get_week_num(day=begin.day),
-    )
-    week_end: Final = Week(
-        year=end.year,
-        month=end.month,
-        week_in_month=Week.get_week_num(day=end.day),
-    )
-    week_now: Final = Week(
-        year=now.year,
-        month=now.month,
-        week_in_month=Week.get_week_num(day=now.day),
-    )
-
-    yield from (
-        compute_calendar_year(
-            week_begin=week_begin,
-            week_end=week_end,
-            week_now=week_now,
-            year=year,
-        )
-        for year in range(begin.year, end.year + 1)
-    )
-
-
-def compute_calendar_year(
-    *,
-    week_begin: Week,
-    week_end: Week,
-    week_now: Week,
-    year: int,
-) -> STATUS_YEAR:
-    yield from (
-        compute_calendar_month(
-            week_begin=week_begin,
-            week_end=week_end,
-            week_now=week_now,
-            year=year,
-            month=month,
-        )
-        for month in range(MONTHS_IN_YEAR)
-    )
-
-
-def compute_calendar_month(
-    *,
-    week_begin: Week,
-    week_end: Week,
-    week_now: Week,
-    year: int,
-    month: int,
-) -> STATUS_MONTH:
-    yield from (
-        compute_calendar_week(
-            week_begin=week_begin,
-            week_end=week_end,
-            week_now=week_now,
-            week_cur=Week(
-                year=year,
-                month=month,
-                week_in_month=week,
-            ),
-        )
-        for week in range(WEEKS_IN_MONTH)
-    )
-
-
-def compute_calendar_week(
-    *,
-    week_begin: Week,
-    week_end: Week,
-    week_now: Week,
-    week_cur: Week,
-) -> STATUS_WEEK:
-    return week_cur < week_begin or week_cur > week_now or week_cur > week_end
-
-
-def render_calendar(
-    *,
-    years: Iterable[STATUS_YEAR],
-    year_begin: int,
-    labels: bool,
-    delay: float,
-) -> None:
-    if labels:
-        print(
-            functools.reduce(
-                lambda s, m: f"{s}  {m.upper()}",
-                CALENDAR_MONTH_ABBR,
-                "    ",
-            )
-        )
-
-    render_week = functools.partial(
-        print,
-        end="",
-        flush=delay > 0.0,
-    )
-
-    for label, year in enumerate(
-        iterable=years,
-        start=year_begin,
-    ):
-        if labels:
-            print(label, end=" ")
-
-        for month in year:
-            for week_done in month:
-                render_week(
-                    RENDER_CHARS[week_done],
-                )
-                time.sleep(delay)
-
-            print(end=" ")
-
-        print()
-
-
-if __name__ == "__main__":
+def main() -> None:
     WEEKS: Final = int(
         os.environ.get(
             "WEEKS",
@@ -199,13 +73,167 @@ if __name__ == "__main__":
         day=DAY_BEGIN,
     )
 
+    calendar: Final = compute_calendar(
+        begin=BEGIN,
+        end=BEGIN + datetime.timedelta(days=WEEKS * DAYS_IN_WEEK),
+        now=datetime.datetime.now(),
+        days_in_week=DAYS_IN_WEEK,
+        weeks_in_month=WEEKS_IN_MONTH,
+        months_in_year=MONTHS_IN_YEAR,
+    )
+
     render_calendar(
-        years=compute_calendar(
-            begin=BEGIN,
-            end=BEGIN + datetime.timedelta(days=WEEKS * DAYS_IN_WEEK),
-            now=datetime.datetime.now(),
-        ),
+        years=calendar,
         year_begin=BEGIN.year,
         labels=LABELS,
+        header=CALENDAR_MONTH_ABBR,
         delay=DELAY,
+        render_chars=RENDER_CHARS,
     )
+
+
+def compute_calendar(
+    *,
+    begin: datetime.datetime,
+    end: datetime.datetime,
+    now: datetime.datetime,
+    days_in_week: int,
+    weeks_in_month: int,
+    months_in_year: int,
+) -> Generator[STATUS_YEAR]:
+    get_week_num: Final = functools.partial(
+        Week.get_week_num,
+        days_in_week=days_in_week,
+        weeks_in_month=weeks_in_month,
+    )
+
+    week_begin: Final = Week(
+        year=begin.year,
+        month=begin.month,
+        week_in_month=get_week_num(day=begin.day),
+    )
+    week_end: Final = Week(
+        year=end.year,
+        month=end.month,
+        week_in_month=get_week_num(day=end.day),
+    )
+    week_now: Final = Week(
+        year=now.year,
+        month=now.month,
+        week_in_month=get_week_num(day=now.day),
+    )
+
+    yield from (
+        compute_calendar_year(
+            week_begin=week_begin,
+            week_end=week_end,
+            week_now=week_now,
+            year=year,
+            weeks_in_month=weeks_in_month,
+            months_in_year=months_in_year,
+        )
+        for year in range(begin.year, end.year + 1)
+    )
+
+
+def compute_calendar_year(
+    *,
+    week_begin: Week,
+    week_end: Week,
+    week_now: Week,
+    year: int,
+    weeks_in_month: int,
+    months_in_year: int,
+) -> STATUS_YEAR:
+    yield from (
+        compute_calendar_month(
+            week_begin=week_begin,
+            week_end=week_end,
+            week_now=week_now,
+            year=year,
+            month=month,
+            weeks_in_month=weeks_in_month,
+        )
+        for month in range(months_in_year)
+    )
+
+
+def compute_calendar_month(
+    *,
+    week_begin: Week,
+    week_end: Week,
+    week_now: Week,
+    year: int,
+    month: int,
+    weeks_in_month: int,
+) -> STATUS_MONTH:
+    yield from (
+        compute_calendar_week(
+            week_begin=week_begin,
+            week_end=week_end,
+            week_now=week_now,
+            week_cur=Week(
+                year=year,
+                month=month,
+                week_in_month=week,
+            ),
+        )
+        for week in range(weeks_in_month)
+    )
+
+
+def compute_calendar_week(
+    *,
+    week_begin: Week,
+    week_end: Week,
+    week_now: Week,
+    week_cur: Week,
+) -> STATUS_WEEK:
+    return week_cur < week_begin or week_cur > week_now or week_cur > week_end
+
+
+def render_calendar(
+    *,
+    years: Iterable[STATUS_YEAR],
+    year_begin: int,
+    labels: bool,
+    header: Sequence[str],
+    delay: float,
+    render_chars: Mapping[STATUS_WEEK, str],
+) -> None:
+    if labels:
+        print(
+            functools.reduce(
+                lambda s, m: f"{s}  {m.upper()}",
+                header,
+                "    ",
+            )
+        )
+
+    render_week: Final = functools.partial(
+        print,
+        end="",
+        flush=delay > 0.0,
+    )
+
+    for label, year in enumerate(
+        iterable=years,
+        start=year_begin,
+    ):
+        if labels:
+            print(label, end=" ")
+
+        for month in year:
+            for week_done in month:
+                render_week(
+                    render_chars[week_done],
+                )
+                time.sleep(delay)
+
+            print(end=" ")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()
